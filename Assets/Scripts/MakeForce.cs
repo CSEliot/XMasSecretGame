cca38f40963b5e91949f86b1fe505368a9856564
Eliot <Eliot@localhost>
2014-12-15 02:11:09 -0500

    THINGS FIRST COMMIT

---
+++ Assets/Scripts/MakeForce.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeForce : MonoBehaviour {
+
+	float rotateZ;
+	public Rigidbody body;
+	public float forceAmount = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//rigidbody.AddRelativeForce (transform.forward);
+		body.AddForceAtPosition (transform.forward*-forceAmount,transform.position, ForceMode.Force);
+	}
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Manager {
+
+	public const bool DEBUG = true;
+	private const string myName = "Eliot";
+	//change to your name above, then whenever you want to see your own errors, call the function with your name.
+
+
+	// BUT, if you think the error is relevant to everyone, make the second string say 'always' instead.
+	public static void say(string msg, string speaker){
+		if(DEBUG && (speaker == myName || speaker == "always")){
+			Debug.Log(msg);
+		}
+
+	}
+}
