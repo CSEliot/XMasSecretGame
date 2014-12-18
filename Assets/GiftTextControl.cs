25eae748eb714ad4ca0455cbd12e087381794b19
Eliot <Eliot@localhost>
2014-12-17 23:17:24 -0500

    Forgot some things

---
+++ Assets/GiftTextControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GiftTextControl : MonoBehaviour {
+
+    private int giftCount = 0;
+    private string GiftString;
+    private Text presentsText;
+	// Use this for initialization
+	void Start () {
+        GiftString = "Presents delivered to Unity-Chan: ";
+        presentsText = gameObject.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        presentsText.text = (GiftString + giftCount);
+	}
+
+
+    public void AddGiftCount()
+    {
+        giftCount++;
+    }
+
+    public void SubtractGiftCount()
+    {
+        giftCount--;
+    }
+}
---
+++ Assets/WaitForGifts.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitForGifts : MonoBehaviour {
+
+    public GameObject giftsText;
+    private bool gottenAGift = false;
+    private float finalTime;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider thing)
+    {
+        Manager.say("IN DELIVERY SPACE: " + thing.name, "Eliot");
+        if (!gottenAGift) { finalTime = Time.timeSinceLevelLoad; gottenAGift = true; }
+        if (thing.gameObject.layer == 10)
+        {
+            giftsText.GetComponent<GiftTextControl>().AddGiftCount();
+        }
+    }
+
+    void OnTriggerExit(Collider thing)
+    {
+        Manager.say("OUT DELIVERY SPACE: " + thing.name, "Eliot");
+
+        if (thing.gameObject.layer == 10)
+        {
+            giftsText.GetComponent<GiftTextControl>().SubtractGiftCount();
+        }
+    }
+}
