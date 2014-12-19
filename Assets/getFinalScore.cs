7be66a46570bca0e9d589cb8a7e4db478519f117
Eliot <Eliot@localhost>
2014-12-19 11:26:57 -0500

    almost finished level 1

---
+++ Assets/getFinalScore.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class getFinalScore : MonoBehaviour {
+
+    public GameObject textController;
+    private Text finalText;
+    private string strText = "FINAL SCORE\n";
+    int highscore;
+	// Use this for initialization
+	void Start () {
+        finalText = gameObject.GetComponent<Text>();
+        finalText.text = strText;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        highscore = textController.GetComponent<GiftTextControl>().getHighScore();
+        Manager.say("Highschore: " + highscore, "Eliot");
+        finalText.text = strText + highscore;
+	}
+}
